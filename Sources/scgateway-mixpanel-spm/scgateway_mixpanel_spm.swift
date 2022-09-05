622bc866c21064aba220456a5e9c17e6e14835c1
ankit-deshmukh <ankit.deshmukh@smallcase.com>
2022-09-05 14:51:16 +0530

    build: added logs

--- Sources/scgateway-mixpanel-spm/scgateway_mixpanel_spm.swift
+++ Sources/scgateway-mixpanel-spm/scgateway_mixpanel_spm.swift
@@ -8,6 +8,8 @@ public struct scgateway_mixpanel_spm {
     }
     
     public static func test() {
+        print("initializing mixpanel")
         Mixpanel.initialize(token: "952676d310d87d9665e5d79f88cc8814")
+        print("Mixpanel initialised")
     }
 }
